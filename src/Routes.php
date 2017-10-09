9d434242fa1a06f345340b1a4ad6d5bd0ad87cfd
Asim Zubair <31400312+ngiasim@users.noreply.github.com>
2017-10-09 20:43:35 +0500

    Update Routes.php

--- src/Routes.php
+++ src/Routes.php
@@ -8,16 +8,17 @@ Route::group([
 		'middleware' => ['auth']
 	], function () {
 	   	
-			Route::get('products/uploadcsv','ngiasim\product_category\ProductController@uploadCSV');
-			Route::post('products/storecsv','ngiasim\product_category\ProductController@storeCSV');
+	Route::get('products/uploadcsv','ngiasim\categories\ProductController@uploadCSV');
+	Route::post('products/storecsv','ngiasim\categories\ProductController@storeCSV');
 
 
-			Route::post('products/addTags','ngiasim\product_category\ProductController@addTags');
-			Route::get('products/removeTags/{id}','ngiasim\product_category\ProductController@removeTags');
+	Route::post('products/addTags','ngiasim\categories\ProductController@addTags');
+	Route::get('products/removeTags/{id}','ngiasim\categories\ProductController@removeTags');
 
 
-            Route::resource('categories','ngiasim\product_category\CategoryController');
-            Route::resource('products','ngiasim\product_category\ProductController');
+        Route::resource('categories','ngiasim\categories\CategoryController');
+        Route::resource('products','ngiasim\categories\ProductController');
+			
 	});
-		});
+    });
 });
