07a38fcd87e1a31bf91ea5622390c318c982c15e
Asim Zubair <31400312+ngiasim@users.noreply.github.com>
2017-10-16 13:06:50 +0500

    Update Log_product_bulk_uploads.php

--- src/models/Log_product_bulk_uploads.php
+++ src/models/Log_product_bulk_uploads.php
@@ -1,30 +1,27 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+namespace App;
 
-class LogProductBulkUploads extends Migration
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Log_product_bulk_uploads extends Model
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('log_product_bulk_uploads', function(Blueprint $table)
-        {
-            $table->increments('log_product_bulk_uploads_id',15);
-            $table->integer('fk_product')->unsigned();
-            $table->integer('fk_bulk_uploads')->unsigned();
-            $table->timestamps();
-            $table->softDeletes();
-        });
+	use SoftDeletes;
+	protected $table = 'log_product_bulk_uploads';
+	protected $primaryKey = "log_product_bulk_uploads_id";
+
+    protected $fillable = ['fk_product','fk_bulk_uploads'];
+
+
+    protected function addProductFileId($request){  
+            $this->fill([
+                'fk_bulk_uploads'       => $request['fk_bulk_uploads'],
+                'fk_product'            => $request['fk_product']
+            ]);
+            $this->save();
     }
 
-    public function down()
-    {
-        Schema::drop('log_product_bulk_uploads');
-    }
 }
+
+
