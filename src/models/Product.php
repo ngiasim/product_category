0c939c73b5d2eefe4ec49ff44128a555980bcd02
s.asad <s.asad@nextgeni.com>
2017-10-24 11:37:25 +0500

    commit changes

--- src/models/Product.php
+++ src/models/Product.php
@@ -18,12 +18,10 @@ class Product extends Model
     {
         return $this->hasOne('App\Models\Product_description', 'fk_product', 'product_id');
     }
-
     public function productsDescriptions()
     {
         return $this->hasMany('App\Models\Product_description', 'fk_product', 'product_id');
     }
-
     public function ProductAttribute()
     {
        return $this->hasMany('App\Models\ProductAttribute', 'fk_product', 'product_id');
@@ -38,7 +36,6 @@ class Product extends Model
     {
        return $this->hasMany('App\Models\MapProductInventoryItem', 'fk_product', 'product_id');
     }
-
     protected function rules($except_id=""){
         $arr =  array(
             'meta_keywords'              => 'required|max:200' ,
@@ -47,7 +44,6 @@ class Product extends Model
             'products_sku'               => 'required|max:200',
             'base_price'                 => 'required|integer'
         );
-
         return $arr;
     }
 
@@ -79,23 +75,17 @@ class Product extends Model
         $products->fk_product_status    = $request['fk_product_status'];
         $products->products_sku         = $request['products_sku'];
         $products->base_price           = $request['base_price'];
-
         $products->save();
     }
-
     protected function updateProductSeo($request){
         $products = $this->find($request['product_id']);
         $products->meta_title           = $request['meta_title'];
         $products->meta_keywords        = $request['meta_keywords'];
         $products->meta_description     = $request['meta_description'];
-
         $products->save();
     }
-
     protected function deleteProducts($id){
         $products = $this->find($id);
         $products->delete();
     }
-
-
 }
