ce541f88d1ec3689d9e424f77f5af3f6cc4ce3ce
asim <asim@nextgeni.com>
2017-10-26 18:19:14 +0500

    routes updated, namespace added at top

--- src/Routes.php
+++ src/Routes.php
@@ -1,46 +1,42 @@
 <?php
 Route::group([
-    'domain' => Config::get('app.domains.cockpit')
+    'namespace' => 'Ngiasim\Categories','domain' => Config::get('app.domains.cockpit')
 ], function () {
 
     Route::group(['middleware' => ['web']], function () {
+		
 		Route::group([
-		'middleware' => ['auth']
-	], function () {
+			'middleware' => ['auth']
+		], function () {
 
 
-		Route::group(['prefix' => 'products'], function () {
+			Route::group(['prefix' => 'products'], function () {
 				//Route::resource('products/inventory','ngiasim\categories\InventoryController');
-				Route::get('inventory/create','ngiasim\categories\InventoryController@create');
-				Route::get('inventory/{id}','ngiasim\categories\InventoryController@show');
-				Route::post('inventory','ngiasim\categories\InventoryController@store');
-				Route::delete('inventory/delete/{id}','ngiasim\categories\InventoryController@destroy');
+				Route::get('inventory/create','InventoryController@create');
+				Route::get('inventory/{id}','InventoryController@show');
+				Route::post('inventory','InventoryController@store');
+				Route::delete('inventory/delete/{id}','InventoryController@destroy');
 
-				Route::get('getproducts','ngiasim\categories\ProductController@getProducts');
-				Route::get('categorization/{id}','ngiasim\categories\ProductController@categorization');
+				Route::get('getproducts','ProductController@getProducts');
+				Route::get('categorization/{id}','ProductController@categorization');
 
-				Route::get('seo/{id}','ngiasim\categories\ProductController@seo');
-				Route::post('updateseo','ngiasim\categories\ProductController@updateSeo');
-				Route::get('attributes/{id}','ngiasim\categories\ProductController@attributes');
-				Route::get('logs/{id}','ngiasim\categories\ProductController@logs');
+				Route::get('seo/{id}','ProductController@seo');
+				Route::post('updateseo','ProductController@updateSeo');
+				Route::get('attributes/{id}','ProductController@attributes');
+				Route::get('logs/{id}','ProductController@logs');
 
-				Route::get('images/{id}','ngiasim\categories\ProductImageController@uploadImages');
-				Route::post('storeimages','ngiasim\categories\ProductImageController@storeImages');
-				Route::get('makedefaultimage/{id}','ngiasim\categories\ProductImageController@makeDefaultImage');
-				Route::post('updateImageType','ngiasim\categories\ProductImageController@updateImageType');
-				Route::post('updateImageSortOrder','ngiasim\categories\ProductImageController@updateImageSortOrder');
-				Route::get('removeimages/{id}','ngiasim\categories\ProductImageController@removeImages');
+				Route::get('images/{id}','ProductImageController@uploadImages');
+				Route::post('storeimages','ProductImageController@storeImages');
+				Route::get('makedefaultimage/{id}','ProductImageController@makeDefaultImage');
+				Route::post('updateImageType','ProductImageController@updateImageType');
+				Route::post('updateImageSortOrder','ProductImageController@updateImageSortOrder');
+				Route::get('removeimages/{id}','ProductImageController@removeImages');
 
-				Route::post('addTags','ngiasim\categories\ProductController@addTags');
-				Route::get('removeTags/{id}','ngiasim\categories\ProductController@removeTags');
-				
-				
+				Route::post('addTags','ProductController@addTags');
 			});
-
-        Route::resource('categories','ngiasim\categories\CategoryController');
-        Route::resource('products','ngiasim\categories\ProductController');
-
-
-	});
+			Route::resource('products','ProductController');
+			Route::resource('categories','CategoryController');
+       		
+		});
     });
 });
